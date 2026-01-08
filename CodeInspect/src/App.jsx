3fb39fa3d40fe6c9505abeb9ccd1da2984c48165
AdityaVerma810 <adicode810@gmail.com>
2026-01-08 20:30:51 +0530

    created navbar for my website and added initial editor window

--- CodeInspect/src/App.jsx
+++ CodeInspect/src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import "./App.css"
-import Navbar from './components/Navbar'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Editor from '@monaco-editor/react';
 
 const App = () => {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar/>
+      <div className="main flex justify-between" style={{ height: "calc(100vh - 90px" }}>
+        <div className="left h-[87.5%] w-[50%]">
+             <Editor height="100%" theme='vs-dark' language="javascript" value="// some comment"  />
+        </div>
+      </div>
+       
   
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- CodeInspect/src/components/Navbar.jsx
+++ CodeInspect/src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Snail } from 'lucide-react';
+import { LoaderPinwheel } from 'lucide-react';
+
 
 const Navbar = () => {
   return (
 	<>
-	  
+	  <div className="nav flex items-center justify-between h-[90px] bg-zinc-900" style={{padding:"0px 150px"}}>
+		<Snail />
+		<div className="logo flex items-center gap-[10px]">
+          <Snail size={30} color='#33ea8f'/>
+          <span className="text-2xl font-bold text-white ml-2">Code Inspect</span>
+        </div>
+		 <div className="icons flex items-center gap-[20px]">
+          <i className='cursor-pointer transition-all hover:text-[#33ea8f]'><LoaderPinwheel/></i>
+        </div>
+	  </div>
 	</>
   )
 }
 
 export default Navbar
+ 
